08356697316a35ef194c7e336f36a827ed4ebbb6
Abdulaziz <abdulazizisa579@gmail.com>
2023-12-06 12:29:39 -0800

    3rd push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
+from sys import argv
+arg_str_0 = "arguments."
+arg_str_1 = "argument:"
+arg_str_n = "arguments:"
+argc = len(argv) - 1
 
-import sys
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+def print_args():
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    if (argc == 0):
+        print("{:d} {}".format(argc, arg_str_0))
+    elif (argc == 1):
+        print("{:d} {}".format(argc, arg_str_1))
+        print_args()
+    else:
+        print("{:d} {}".format(argc, arg_str_n))
+        print_args()
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
-add = 0
-for a in argv[1:]:
-    add += int(a)
-print("{:d}".format(add))
+argc = len(argv) - 1
+
+
+def sum_args():
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    return sum
+
+
+if __name__ == "__main__":
+    if (argc == 0):
+        print("{:d}".format(argc))
+    else:
+        print("{:d}".format(sum_args()))
